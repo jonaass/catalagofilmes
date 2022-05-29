80b5912329738ca2d158f173290366edb508f290
Jonas da Cunha Madeira <jonascn.2021@gmail.com>
2022-05-29 16:30:39 -0300

    usuario

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config' 
-import { con } from './repository/connection.js'
+
+import usuarioController from './controller/usuarioController.js'
 
 import express from 'express'
 import cors from 'cors'
@@ -9,6 +10,8 @@ server.use(cors());
 server.use(express.json());
 
 
+//configuração dos Endpoints
+server.use(usuarioController);
 
 
 
